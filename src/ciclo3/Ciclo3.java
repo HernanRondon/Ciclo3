76f027e60e997f8842313c6a71000d5ceb648772
HernanRondon <rondonhernan@gmail.com>
2022-09-14 21:07:49 -0500

    se agrega el proyeto

---
+++ src/ciclo3/Ciclo3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ciclo3;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Ciclo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 5;
+        int y = 6;
+        int suma = x + y;
+        System.out.println("El resultado de la suma es: " + suma);
+    }
+    
+}
